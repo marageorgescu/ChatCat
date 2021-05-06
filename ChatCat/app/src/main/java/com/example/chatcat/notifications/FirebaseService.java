a1d7343f9a01856d9cd56582badca33bc9288bd1
marageorgescu <49337453+marageorgescu@users.noreply.github.com>
2021-05-06 23:33:27 +0300

    debugging firebaseservice deprecated ;)

--- ChatCat/app/src/main/java/com/example/chatcat/notifications/FirebaseService.java
+++ ChatCat/app/src/main/java/com/example/chatcat/notifications/FirebaseService.java
@@ -1,21 +1,22 @@
 package com.example.chatcat.notifications;
 
+import androidx.annotation.NonNull;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
 
-public class FirebaseService extends FirebaseInstanceIdService {
+public class FirebaseService extends FirebaseMessagingService {
+
+
     @Override
-    public void onTokenRefresh() {
-        super.onTokenRefresh();
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        String tokenRefresh = FirebaseInstanceId.getInstance().getToken();
-        if(user != null) {
-            updateToken(tokenRefresh);
-        }
+    public void onNewToken(@NonNull String s) {
+        super.onNewToken(s);
+        updateToken(s);
     }
 
     private void updateToken(String tokenRefresh) {
